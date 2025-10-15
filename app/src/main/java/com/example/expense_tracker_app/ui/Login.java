45cd3661afb1dfc4d1738857f973c7db4a1930e5
duy321111 <duy321111@gmail.com>
2025-10-15 14:20:24 +0700

    chore(structure): initialize MVVM project structure

--- app/src/main/java/com/example/expense_tracker_app/ui/Login.java
+++ app/src/main/java/com/example/expense_tracker_app/ui/Login.java
@@ -1,12 +1,11 @@
-package com.example.expense_tracker_app;
+package com.example.expense_tracker_app.ui;
 
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.expense_tracker_app.R;
 
 public class Login extends AppCompatActivity {
 
--- app/src/main/java/com/example/expense_tracker_app/ui/MainActivity.java
+++ app/src/main/java/com/example/expense_tracker_app/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.expense_tracker_app;
+package com.example.expense_tracker_app.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.expense_tracker_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
